505a58b8af7f31781e01921dd2ca9e6cd187d62d
Alex <Lam.Alexander@hotmail.com>
2024-04-10 16:42:34 -0600

    Added a bit of styling to login page

--- app/components/UserInfo.jsx
+++ app/components/UserInfo.jsx
@@ -16,10 +16,13 @@ const UserInfo = () => {
 
   // Display login button if not authenticated
   return (
+    <div className='border-solid bg-slate-100 p-11 rounded-xl '>
+      <p>Welcome to TalentoTrack</p>
     <button className='flex items-center gap-4 shadow-xl rounded-lg pl-3' onClick={()=> signIn('google')}>
         <Image src="/google-logo.png" alt="Google icon" width={30} height={30} />
         <span className='bg-blue-500 text-white px-4 py-3'>Sign in with Google</span>
         </button>
+    </div>
   );
 };
 
--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,22 @@
 import UserInfo from "@/app/components/UserInfo";
 import Layout from "@/app/layoutLogin";
+import Header from "@/app/components/Header";
+import Sidebar from "@/app/components/Sidebar";
+import Footer from "@/app/components/Footer";
 
 
 export default function Home() {
   return (
-    
-    <div className="grid place-items-center h-screen -mt-24">
-      <UserInfo/>
-      {/* <Layout/> */}
+    <div className="bg-gray-200 ">
+      <div className="flex flex-col justify-center items-center min-h-screen ">
+        <Header/>
+          <div className="flex flex-col items-center justify-center flex-grow">
+            <UserInfo/>
+          </div>
+      </div>
+      <Footer/>
     </div>
   );
 }
+
+
