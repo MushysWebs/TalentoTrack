f40d3553e1448b6d379ab4aa606c7559ce65bf13
Alex <Lam.Alexander@hotmail.com>
2024-04-11 19:24:37 -0600

    Finished Secured Pages

--- app/components/Header.js
+++ app/components/Header.js
@@ -7,7 +7,9 @@ const Header = () => {
   return (
     <div className="bg-gradient-to-r from-gray-300 to-gray-600 m-0 p-0">
       <div className="flex justify-between items-center">
-        <img src="/TalentLogo.png" alt="Logo" className="w-1/5 mt-2 mb-2 ml-2 rounded-2xl" />
+        <a href="/">
+            <img src="/TalentLogo.png" alt="Logo" className="w-1/5 mt-2 mb-2 ml-2 rounded-2xl" />
+        </a>
         <Navbar /> 
       </div>
     </div>
--- app/page.js
+++ app/page.js
@@ -1,17 +1,8 @@
-'use client'
-import AddPost from "./components/AddPost";
-import PostList from "./components/PostList";
-import LogInBtn from "./components/LogInBtn";
-import { getSession } from "next-auth/react";
-
-
-async function getData() {
-  const res = await fetch('https://talento-track.vercel.app/api/posts', {cache: "no-cache"})
-  if(!res.ok) {
-    throw new Error("Failed to fetch data")
-  }
-  return res.json();
-}
+// 'use client'
+// import { useState, useEffect } from 'react';
+// import AddPost from "./components/AddPost";
+// import PostList from "./components/PostList";
+// import { getSession } from "next-auth/react";
 
 // async function getData() {
 //   const res = await fetch('http://localhost:3000/api/posts', {cache: "no-cache"})
@@ -21,21 +12,133 @@ async function getData() {
 //   return res.json();
 // }
 
-const page = async () => {
-  
-  try {
-    const posts = await getData();
-    console.log(posts);
-    return (
-      <main>
-        <AddPost />
-        <PostList post={posts} />
-      </main>
-    );
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return <div>Error fetching data</div>;
+// const Page = () => {
+//   const [session, setSession] = useState(null);
+//   const [posts, setPosts] = useState(null);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const userSession = await getSession();
+//       setSession(userSession);
+      
+//       if (userSession) {
+//         try {
+//           const postsData = await getData();
+//           setPosts(postsData);
+//         } catch (error) {
+//           setError(error);
+//         }
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   if (!session) {
+//     return (
+//       <div className="flex items-center justify-center h-screen">
+//       <div className="text-center">
+//         {/* Display the message to prompt the user to log in */}
+//         <p className="text-lg font-semibold mb-2">Please log in to access this feature.</p>
+//       </div>
+//     </div>
+//     );
+//   }
+
+//   if (error) {
+//     return <div>Error fetching data: {error.message}</div>;
+//   }
+
+//   if (!posts) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <main>
+//       <AddPost />
+//       <PostList post={posts} />
+//     </main>
+//   );
+// }
+
+// export default Page;
+
+'use client'
+import { useState, useEffect } from 'react';
+import AddPost from "./components/AddPost";
+import PostList from "./components/PostList";
+import { getSession } from "next-auth/react";
+
+// async function getData() {
+//   const res = await fetch('https://talento-track.vercel.app/api/posts', {cache: "no-cache"})
+//   if(!res.ok) {
+//     throw new Error("Failed to fetch data")
+//   }
+//   return res.json();
+// }
+
+async function getData() {
+  const res = await fetch('http://localhost:3000/api/posts', {cache: "no-cache"})
+  if(!res.ok) {
+    throw new Error("Failed to fetch data")
   }
+  return res.json();
 }
 
-export default page;
+const Page = () => {
+  const [session, setSession] = useState(null);
+  const [posts, setPosts] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const userSession = await getSession();
+      setSession(userSession);
+
+      if (userSession) {
+        try {
+          const postsData = await getData();
+          setPosts(postsData);
+        } catch (error) {
+          setError(error);
+        }
+      }
+    };
+
+    fetchData();
+
+    // Polling to refresh data every 1 seconds
+    const intervalId = setInterval(fetchData, 1000);
+
+    return () => clearInterval(intervalId); // Cleanup interval on unmount
+  }, []);
+
+  if (!session) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        {/* Display the message to prompt the user to log in */}
+        <p className="text-lg font-semibold mb-2">Please log in to access this feature.</p>
+      </div>
+    </div>
+    );
+  }
+
+  if (error) {
+    return <div>Error fetching data: {error.message}</div>;
+  }
+
+  if (!posts) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <main>
+      <AddPost />
+      <PostList post={posts} />
+    </main>
+  );
+}
+
+export default Page;
