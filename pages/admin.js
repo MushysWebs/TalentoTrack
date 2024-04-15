6e681a4dd35880bcc542d0b0eae2bd0da2fa1f0d
Alex <Lam.Alexander@hotmail.com>
2024-04-15 15:33:55 -0600

    Updating Admin Access 3

--- pages/admin.js
+++ pages/admin.js
@@ -36,7 +36,7 @@ const Admin = () => {
   }
 
   // Approved emails that can access the admin page
-  const approvedEmails = ["lam.alexander@hotmail.com", "gradyspurrill@gmail.com"];
+  const approvedEmails = ["lam.alexander@hotmail.com", "gradyspurrill@gmail.com", "", ""];
 
   // Check if the user's email is in the list of approved emails
   if (!approvedEmails.includes(session.user.email)) {
